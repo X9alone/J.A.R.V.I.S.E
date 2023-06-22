c3fc29c7fde7a88f9990d1658e73686a1c9ff1a4
X9alone <dimakadusko@gail.com>
2023-06-22 18:36:06 +0200

    add task

--- main.py
+++ main.py
@@ -14,3 +14,32 @@ def talk(words):
 
 
 talk("Hi, can i help you?")
+
+
+def command():
+    r = sr.Recognizer()
+    with sr.Microphone as source:
+        print("Say")
+        r.pause_threshold = 1
+        r.adjust_for_ambient_noise(source, duration=1)
+        audio = r.listen(source)
+
+    try:
+        task = r.recognize_google(audio, language="ua-UA"),lower()
+        print("ви проговорили: " + task)
+    except sr.UnknownValueError:
+        talk("Я вас не зрозумів")
+        task = command()
+
+    return task
+
+
+def make_something(task):
+    if "відкрий сайт" in task:
+        talk("відкриваю")
+        url = "https://ituniver.com"
+        webbrowser.open(url)
+
+
+while True:
+    make_something(task=command())
